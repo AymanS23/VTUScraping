9f7c90d638a94f3370aae214fe561ece758de6ac
unknown <sher.ayman@gmail.com>
2019-05-03 17:20:12 +0530

    Second

--- crawler.py
+++ crawler.py
@@ -5,31 +5,36 @@ from PIL import Image
 import re
 import math
 import urllib
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 class captchaclass:
     
     def solve_captcha(self):
         #BREAK CAPTCHA
-        #img = driver.find_elements(By.TAG_NAME, "img")
-        #src = img[1].get_attribute('src')
-        print("##################################################")
-        #print(src)
+        driver = webdriver.Chrome(executable_path='C:\\Users\\GV62\\Downloads\\selechrome\\chromedriver_win32\\chromedriver.exe')
+        driver.get("http://results.vtu.ac.in/resultsvitavicbcs_19/index.php")
+        img = driver.find_elements(By.TAG_NAME, "img")
+        src = img[1].get_attribute('src')
+        #print("##################################################")
+        print(src)
         loc = img[1].location
         size = img[1].size
-        #print(loc,size)
-        #driver.save_screenshot('ss.png')
+        print(loc,size)
+        driver.save_screenshot('ss.png')
         im = Image.open('ss.png')
         left = loc['x']
         top = loc['y']
         right = loc['x'] + size['width']
         bottom = loc['y'] + size['height']
         im = im.crop((left,top,right,bottom))
-        #im.show()
+        im.show()
         im.save('ss.png')
         try:
             captcha_text = pytesseract.image_to_string(Image.open('ss.png'))
         except:
             print("captcha string error")
-        #print(captcha_text)
+        print(captcha_text)
         return captcha_text
         #captcha = driver.find_element(By.NAME,'captchacode')
         #captcha.clear()
